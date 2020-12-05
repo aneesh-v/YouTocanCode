7be374bdf11f103919efa4a0be78579589b4322c
Aneesh vijayachandran <aneeshv28@gmail.com>
2020-12-05 21:56:45 +0530

    removed try-catch from routes, Implemented custom error handling, asyncHandler, and GeoJSON using node-geocoder

---
+++ middleware/errorHandler.js
@@ -0,0 +1,33 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+   let error = { ...err };
+   error.message = err.message;
+
+   // log to Developer
+   //    console.log(err);
+
+   // Mongoose bad ObjectId
+   if (err.name === 'CastError') {
+      const message = `Resource not found with id of ${err.value}`;
+      error = new ErrorResponse(message, 404);
+   }
+
+   //Mongoose document duplicate error
+   if (err.code === 11000) {
+      const message = 'Duplicate collection found in database.';
+      error = new ErrorResponse(message, 400);
+   }
+   //Mongoose Validation Error
+   if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map((val) => val.message);
+      error = new ErrorResponse(message, 400);
+   }
+
+   res.status(error.statusCode || 500).json({
+      success: false,
+      error: error.message || 'Unidentified Server Error',
+   });
+};
+
+module.exports = errorHandler;
--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-const express = require('express');
-const dotenv = require('dotenv');
-
 //Load env vars
+const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 
+const express = require('express');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+const bootcamps = require('./Routes/bootcamps');
+const errorHanlder = require('./middleware/errorHandler');
 const app = express();
+
+// Connect to DB
+connectDB();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middlewre
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/bootcamps', bootcamps);
+app.use(errorHanlder);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      .brightYellow
+  )
 );
+
+// Handle unhandledRejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit
+  server.close(() => process.exit(1));
+});
